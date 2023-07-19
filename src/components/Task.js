2d290b2f6c6acd1fca55ef70344bd21a41c1b587
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-20 02:35:57 +0300

    displayed the list and implemented the delete functionality

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function Task() {
+function Task({ task, deletList }) {
+  const { text, category, id } = task;
+  // console.log(index);
+
+
+  
+  function deleteHandle(id) {
+    deletList(id);
+  }
+
+
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button
+        className="delete"
+        onClick={() => {
+          deleteHandle(id);
+        }}
+      >
+        X
+      </button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,42 @@
 import React from "react";
+import Task from "./Task";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks }) {
+  // console.log(tasks);
+  const [TaskListState, setTaskListState] = useState(tasks);
+
+  //map through the tasks array and send each objecct to TASK returning an array of spans
+  const task = TaskListState.map((task) => {
+    //copy the tasks objects and add an id key to with value set to the object index
+    const newTaskObject = { ...task, id: uuidv4() };
+    console.log(newTaskObject.id);
+    return (
+      <span key={newTaskObject.id}>
+        <Task task={newTaskObject} deletList={deletList} />
+      </span>
+    );
+  });
+
+  function deletList(id) {
+    // console.log(id);
+    const taskCopy = [...task];
+    // console.log(taskCopy[id].key);
+    const filtered = taskCopy.filter(
+      (thisObject) => thisObject.props.children.props.task.id != id
+    );
+    console.log(filtered);
+    setTaskListState(filtered);
+  }
+
+  /**function deletList(id) {
+  const updatedTasks = task.filter((taskSpan) => taskSpan.key !== id.toString());
+  setTaskListState(updatedTasks.map((taskSpan, index) => React.cloneElement(taskSpan, { key: index })));
+}
+ */
+  console.log(task);
+  return <div className="tasks">{task}</div>;
 }
 
 export default TaskList;
