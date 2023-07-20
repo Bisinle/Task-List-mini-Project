756382ebc85fcd7b214d3c271b7349f77e519a9f
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-20 12:09:37 +0300

    displayed the category buttons and applied the equired CSS className

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ CATEGORIES }) {
+  const [SelectedCategory, setSelectedCategory] = useState(null);
+  const [buttonClassName, setButtonClassName] = useState("");
+  // console.log(CATEGORIES);
+  function btnHandle(category) {
+    setSelectedCategory(category);
+  }
+
+
+  const categoryButtons = CATEGORIES.map((category) => {
+    return (
+      <button
+        className={category === SelectedCategory ? "selected" : ""}
+        key={category}
+        onClick={() => btnHandle(category)}
+      >
+        {category}
+      </button>
+    );
+  });
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+
+      {categoryButtons}
     </div>
   );
 }
